eca03dba36934a69c8be3a82fadc108894381b54
Arqam Ahmad Siddiqui <62327688+arqam365@users.noreply.github.com>
2023-11-21 07:47:09 +0530

    emergency contacts added

--- app/src/main/java/com/nextlevelprogrammers/cherry1/EmergencyContactsActivity.kt
+++ app/src/main/java/com/nextlevelprogrammers/cherry1/EmergencyContactsActivity.kt
@@ -1,74 +1,156 @@
 package com.nextlevelprogrammers.cherry1
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.widget.Button
+import android.view.View
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class EmergencyContactsActivity : AppCompatActivity() {
-    private val selectedContacts = mutableListOf<String>()
 
-    private val pickContactLauncher =
-        registerForActivityResult(ActivityResultContracts.PickContact()) { contactUri ->
-            contactUri?.let {
-                contentResolver.query(it, null, null, null, null)?.use { c ->
-                    if (c.moveToFirst()) {
-                        val displayNameIndex = c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
-                        val contactIdIndex = c.getColumnIndex(ContactsContract.Contacts._ID)
+    private val CONTACTS_READ_REQUEST = 101
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emergency_contacts)
 
-                        if (displayNameIndex >= 0) {
-                            val contactName = c.getString(displayNameIndex)
-                            val contactId = c.getString(contactIdIndex)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                CONTACTS_READ_REQUEST
+            )
+        } else {
+            getContacts()
+        }
 
-                            // Get phone number based on contact ID
-                            contentResolver.query(
+    }
+
+    fun onPickContactsClicked(view: View) {
+        val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+        startActivityForResult(intent, PICK_CONTACT_REQUEST)
+    }
+
+    fun onAddContactClicked(view: View) {
+        val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+        startActivityForResult(intent, PICK_CONTACT_REQUEST)
+    }
+
+    companion object {
+        private const val PICK_CONTACT_REQUEST = 102
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_CONTACT_REQUEST && resultCode == Activity.RESULT_OK) {
+            val contactData = data?.data
+
+            contactData?.let {
+                val cursor = contentResolver.query(it, null, null, null, null)
+
+                cursor?.let {
+                    val displayNameIndex = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                    val idIndex = it.getColumnIndex(ContactsContract.Contacts._ID)
+
+                    if (displayNameIndex != -1 && idIndex != -1) {
+                        if (it.moveToFirst()) {
+                            val name = it.getString(displayNameIndex)
+                            val contactId = it.getString(idIndex)
+
+                            val phoneCursor = contentResolver.query(
                                 ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                                 null,
                                 ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                                 arrayOf(contactId),
                                 null
-                            )?.use { phoneC ->
-                                if (phoneC.moveToFirst()) {
-                                    val phoneNumberIndex =
-                                        phoneC.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                            )
 
-                                    // Check if PHONE_NUMBER column exists
-                                    if (phoneNumberIndex >= 0) {
-                                        val phoneNumber = phoneC.getString(phoneNumberIndex)
+                            phoneCursor?.let { phoneCur ->
+                                val phoneNumberIndex =
+                                    phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                                if (phoneCur.moveToFirst() && phoneNumberIndex != -1) {
+                                    val phoneNumber = phoneCur.getString(phoneNumberIndex)
 
-                                        // Do something with the phoneNumber, e.g., add it to the list
-                                        if (selectedContacts.size < 10 && !selectedContacts.contains(contactName)) {
-                                            selectedContacts.add("$contactName: $phoneNumber")
-                                        }
-                                    }
+                                    // Update the CardView with contact information
+                                    findViewById<TextView>(R.id.nameTextView).text = "Name: $name"
+                                    findViewById<TextView>(R.id.numberTextView).text = "Number: $phoneNumber"
                                 }
+                                phoneCur.close()
                             }
-                        } else {
-                            // Handle the case where DISPLAY_NAME column doesn't exist
                         }
+                        it.close()
+                    } else {
+                        // Handle case where columns are not found
                     }
                 }
             }
-
-            // Update the TextView with selected contacts
-            val selectedContactsTextView: TextView = findViewById(R.id.selectedContactsTextView)
-            selectedContactsTextView.text = selectedContacts.joinToString(", ")
-            val contactNumbers: TextView = findViewById(R.id.contactNumber)
-            contactNumbers.text = selectedContacts.joinToString("\n")
         }
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_emergency_contacts)
 
-        val pickContactButton: Button = findViewById(R.id.pickContactButton)
 
-        pickContactButton.setOnClickListener {
-            pickContactLauncher.launch(null)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CONTACTS_READ_REQUEST) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getContacts()
+            }
+        }
+    }
+
+    private fun getContacts() {
+        val contentResolver: ContentResolver = contentResolver
+        val cursor = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        cursor?.let {
+            val nameIndex = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+            val idIndex = it.getColumnIndex(ContactsContract.Contacts._ID)
+
+            while (it.moveToNext()) {
+                val name = if (nameIndex != -1) it.getString(nameIndex) else ""
+                val contactId = if (idIndex != -1) it.getString(idIndex) else ""
+
+                val phoneCursor = contentResolver.query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                    arrayOf(contactId),
+                    null
+                )
+
+                phoneCursor?.let { phoneCur ->
+                    if (phoneCur.moveToFirst()) {
+                        val phoneIndex = phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                        val phoneNumber = if (phoneIndex != -1) phoneCur.getString(phoneIndex) else ""
+
+                        // Do something with name and phoneNumber (e.g., display, store, etc.)
+                        println("Name: $name, Phone Number: $phoneNumber")
+                    }
+                    phoneCur.close()
+                }
+            }
+            it.close()
         }
     }
 }
